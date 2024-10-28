bb811b594cafd8ef2c684567c8a11249d8d1ce01
Yulissa22 <ec1413220@gmail.com>
2024-10-28 10:09:17 -0600

    Agregar archivos de proyecto.

---
+++ HYCMEYCD.RaizCuadrada.UnitTest/RaizCuadradaTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HYCMEYCD20241028;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYCMEYCD20241028.Tests
+{
+    [TestClass()]
+    public class RaizCuadradaTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CalcularRaizCuadrada_Negativo_LanzaExcepcion()
+        {
+            RaizCuadrada raizCuadrada = new RaizCuadrada();
+            raizCuadrada.Raiz(-2);
+        }
+
+        [TestMethod]
+        public void CalcularRaizCuadrada_Positivo_DevuelveRaizCuadrada()
+        {
+            RaizCuadrada raizCuadrada = new RaizCuadrada();
+            double result = raizCuadrada.Raiz(4);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void CalcularRaizCuadrada_Cero_DevuelveCero()
+        {
+            RaizCuadrada raizCuadrada = new RaizCuadrada();
+            double result = raizCuadrada.Raiz(0);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ HYCMEYCD20241028/RaizCuadrada.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HYCMEYCD20241028
+{
+    public class RaizCuadrada
+    {
+        public double Raiz(double numero)
+        {
+            if (numero < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numero), "El número no puede ser negativo.");
+            }
+            return Math.Sqrt(numero);
+        }
+    }
+}
+
+
+
+
